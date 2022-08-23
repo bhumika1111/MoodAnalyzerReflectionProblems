502e1fb51ffc4f72ebebc4e4858da9fd47bd45bc
bhumika1111 <bhoyarpatil@434>
2022-08-23 14:20:35 +0530

    *Check Mood For Happy Or Sad*

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerReflection
+{
+    /// <summary>
+    ///UC1 - Given A Message Ability To Analyse And Respond Happy Or Sad Mood.
+    /// </summary>
+      public class MoodAnalyzer
+        {
+            public string message;
+            public MoodAnalyzer(string message)
+            {
+                this.message = message;
+            }
+            //Analysing the mood by passing happy Or sad.
+            public string AnalyseMood(string message)
+            {
+                if (this.message.Contains("Sad"))
+                {
+                    return "Sad";
+                }
+                else
+                {
+                    return "Happy";
+                }
+            }
+       }
+    
+}
