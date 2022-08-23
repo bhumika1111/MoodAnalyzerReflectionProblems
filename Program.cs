3a9e6cd9a9d9cd92a15aae1ff9ffb3e0ea923b7a
bhumika1111 <bhoyarpatil@434>
2022-08-23 14:12:36 +0530

    *Added Program File For Mood Analyzer Program For Reflection*

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MoodAnalyzerReflection
+{
+  public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("::::::::::Welcome To Mood Analyzer Program Of Reflection Topic:::::::::::::");
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+
+            Console.WriteLine("Please Enter The Message For E.g- Happy or Sad Mood");
+            string message = Console.ReadLine();
+            //Creating Instance Of Class.
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
+            //Referance stored in result .
+           string result =  moodAnalyzer.AnalyseMood(message);
+            Console.WriteLine(result + " " + "MOOD");
+        }
+    }
+}
