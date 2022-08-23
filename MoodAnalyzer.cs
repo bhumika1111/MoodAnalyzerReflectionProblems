8648397126276e95945607142646c13a79058227
bhumika1111 <bhoyarpatil@434>
2022-08-23 17:55:58 +0530

    *Added MoodAnalyser Custom Exception Program File *

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomException
+{
+    /// <summary>
+    /// UC3 -Inform User If Enter Invalid Mood.
+    /// </summary>
+    public class MoodAnalyzer
+    {
+       string message;
+
+        /// <summary>
+        /// Parameterized constructor.
+        /// </summary>
+        /// <param name="message"></param>
+
+        public string AnalyzeMood(string message)
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.EMPTY_MESSAGE, "Mood Should Not Be Empty");
+                }
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NULL_MESSAGE, "Mood Should Not Be Null");
+            }
+        }
+    }
+}
