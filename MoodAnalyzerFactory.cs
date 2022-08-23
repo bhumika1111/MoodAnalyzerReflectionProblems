c837733c76f28ffd3faf75b457457a29fe3c0585
bhumika1111 <bhoyarpatil@434>
2022-08-23 20:34:32 +0530

    *Added MoodAnalyzer Factory File *

---
+++ MoodAnalyzerFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyzerReflection
+{
+   public class MoodAnalyzerFactory
+    {
+      /// <summary>
+      /// UC5 - For parameterised constructor by passing message parameter to the class method.
+      /// </summary>
+      /// <param name="className"></param>
+      /// <param name="constructorName"></param>
+      /// <returns></returns>
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName)
+        {
+            Type type = typeof(MoodAnalyzer);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { "HAPPY" });
+                    return instance;
+
+                }
+                else
+                {
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
+                }
+            }
+            else
+           
+            {
+                throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+            }
+        
+           
+           
+        }
+
+
+    }
+}
